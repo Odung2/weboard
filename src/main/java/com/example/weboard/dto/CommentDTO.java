d37d23e78134f6eda3afddf17544ff6bcb6ce32e
wm-id002518 <kaimjy@kaist.ac.kr>
2024-04-08 18:28:35 +0900

    User API - ResponseEntity<ApiResponse> 로 반환하도록 수정 완

--- src/main/java/com/example/weboard/dto/CommentDTO.java
+++ src/main/java/com/example/weboard/dto/CommentDTO.java
@@ -1,7 +1,10 @@
 package com.example.weboard.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Data
 @NoArgsConstructor
@@ -10,8 +13,15 @@ public class CommentDTO {
     private int postId;
     private int userId;
     private String commentText;
+
     private int createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
     private String createdAt;
+
+
     private Integer updatedBy;
+    @LastModifiedDate
     private String updatedAt;
 }
---
+++ src/main/java/com/example/weboard/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.weboard.exception;
+
+import com.example.weboard.dto.ApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.SignatureException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ApiResponse<Object>> handleJwtSignatureException(SignatureException e){
+        ApiResponse apiResponse= new ApiResponse(400, "토큰의 서명이 올바르지 않습니다.", null);
+        return ResponseEntity.status(400).body(apiResponse);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ApiResponse<Object>> handleExpiredJwtException(ExpiredJwtException e){
+        ApiResponse apiResponse = new ApiResponse(403, "토큰이 만료되었습니다", null);
+        return ResponseEntity.status(403).body(apiResponse);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMalformedJwtException (MalformedJwtException e){
+        ApiResponse apiResponse = new ApiResponse(400, "토큰의 형식이 올바르지 않습니다", null);
+        return ResponseEntity.status(400).body(apiResponse);
+    }
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiResponse<Object>> handleJwtException(JwtException e){
+        ApiResponse apiResponse = new ApiResponse(400, "토큰 처리 중 문제가 생겼습니다", null);
+        return ResponseEntity.status(400).body(apiResponse);
+    }
+}
